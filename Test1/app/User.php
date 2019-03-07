f60bb2e82c77a5daf27a3b5f93b15f1705cb3abf
Do Nguyen Thanh Luan <THANHLUANDONGUYEN@gmail.com>
2019-03-07 13:26:13 +0700

    project cdw1

---
+++ Test1/app/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    // dang nhap class
+    // public function getUser($email) {
+    //     $sql = $this->where('email' -> $email)->get();
+    //     return $sql;
+    // }
+
+    // // luu cai sessection
+    // $this->secssection()->put('email', $row['email']);
+    // return redirect('index');
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
